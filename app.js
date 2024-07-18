704128d96bda3d453f9b186682599150bd5b675b
RmNLcN06 <romain.luciano@orange.fr>
2024-07-18 11:49:04 +0200

    install morgan module

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
+const morgan = require('morgan');
 
 const app = express();
 
+app.use(morgan('tiny'));
 
 app.get('/', (req, res)=>{
     res.send('Message from my app !')
