a1b24bbfca0d17d8de89ebe13e717950fa2d6c9d
RmNLcN06 <romain.luciano@orange.fr>
2024-07-19 15:17:24 +0200

    Add router proto

--- app.js
+++ app.js
@@ -6,12 +6,23 @@ const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+const sessionsRouter = express.Router();
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
+
+sessionsRouter.route('/').get((req, res) => {
+    res.send('It\'s sessions part');
+})
+
+sessionsRouter.route('/1').get((req, res) => {
+    res.send('It\'s a single sessions part');
+})
+
+app.use('/sessions', sessionsRouter);
 
 app.get('/', (req, res)=>
 {
