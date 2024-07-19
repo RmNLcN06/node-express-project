7a0df251805334fa443ec5ba036c0e4dcc08af7e
RmNLcN06 <romain.luciano@orange.fr>
2024-07-19 17:07:22 +0200

    Separate router files

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
-const sessions = require('./src/data/sessions.json');
+const sessionsRouter = require('./src/routers/sessionsRouter');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
-const sessionsRouter = express.Router();
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
@@ -15,18 +14,7 @@ app.use(express.static(path.join(__dirname, '/public/')));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
-sessionsRouter.route('/').get((req, res) => {
-    res.render('sessions', {
-        sessions,
-    });
-})
 
-sessionsRouter.route('/:id').get((req, res) => {
-    const id = req.params.id;
-    res.render('session', {
-        session: sessions[id],
-    });
-})
 
 app.use('/sessions', sessionsRouter);
 
