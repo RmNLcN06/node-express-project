519809b7927dd9aada0c29ae8ae805284b57d105
RmNLcN06 <romain.luciano@orange.fr>
2024-07-22 15:40:43 +0200

    Selecting one session debug => done

--- src/routers/sessionsRouter.js
+++ src/routers/sessionsRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const debug = require('debug')('app:sessionRouter');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 const sessions = require('../data/sessions.json');
 const sessionsRouter = express.Router();
 
@@ -28,9 +28,26 @@ sessionsRouter.route('/').get((req, res) => {
 
 sessionsRouter.route('/:id').get((req, res) => {
     const id = req.params.id;
-    res.render('session', {
-        session: sessions[id],
-    });
+
+    const url = 'mongodb+srv://dbUser:Hollow_Ichigo8891@cluster0.z6d8o6r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+    const dbName = 'globalterms';
+
+    (async function mongo(){
+        let client;
+
+        try {
+            client = await MongoClient.connect(url);
+            debug('Connected to the mongoDB');
+
+            const db = client.db(dbName);
+
+            const session = await db.collection('sessions').findOne({_id: new ObjectId(id)});
+            res.render('session', { session });
+        } catch (error) {
+            debug(error.stack);
+        }
+        client.close();
+    })();
 });
 
 module.exports = sessionsRouter;
