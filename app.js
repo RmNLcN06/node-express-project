fb67643251fd32c5954bdea5e6f7c940be5b33f9
RmNLcN06 <romain.luciano@orange.fr>
2024-07-22 16:59:48 +0200

    Creating Auth Routes

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ const morgan = require('morgan');
 const path = require('path');
 const sessionsRouter = require('./src/routers/sessionsRouter');
 const adminRouter = require('./src/routers/adminRouter');
+const authRouter = require('./src/routers/authRouter');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
 
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
@@ -19,6 +22,7 @@ app.set('view engine', 'ejs');
 
 app.use('/sessions', sessionsRouter);
 app.use('/admin', adminRouter);
+app.use('/auth', authRouter);
 app.get('/', (req, res)=>
 {
     res.render('index', 
