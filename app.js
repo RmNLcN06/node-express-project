5f49679834973d216f650c29fe30e655ebdfc15e
RmNLcN06 <romain.luciano@orange.fr>
2024-07-22 17:25:04 +0200

    Configuring Passport

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
 const path = require('path');
 const sessionsRouter = require('./src/routers/sessionsRouter');
 const adminRouter = require('./src/routers/adminRouter');
@@ -14,6 +18,10 @@ app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
+app.use(cookieParser());
+app.use(session({secret: 'globalterms'}));
+
+require('./src/config/passport.js')(app);
 
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
