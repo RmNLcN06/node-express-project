0a47c519352088ed30e29bcba69f1911c4038143
RmNLcN06 <romain.luciano@orange.fr>
2024-07-24 10:43:10 +0200

    Creating a user profile

--- app.js
+++ app.js
@@ -2,22 +2,21 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
 const passport = require('passport');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 
-const path = require('path');
+const PORT = process.env.PORT || 3000;
+const app = express();
 const sessionsRouter = require('./src/routers/sessionsRouter');
 const adminRouter = require('./src/routers/adminRouter');
 const authRouter = require('./src/routers/authRouter');
 
-const PORT = process.env.PORT || 3000;
-const app = express();
-
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(session({secret: 'globalterms'}));
 
@@ -26,13 +25,11 @@ require('./src/config/passport.js')(app);
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
-
-
 app.use('/sessions', sessionsRouter);
 app.use('/admin', adminRouter);
 app.use('/auth', authRouter);
-app.get('/', (req, res)=>
-{
+
+app.get('/', (req, res) => {
     res.render('index', 
         {
             title: 'GlobalTerms',
@@ -46,6 +43,6 @@ app.get('/', (req, res)=>
     );
 });
 
-app.listen(PORT, ()=>{
-    debug(`Listening to port ${chalk.green(PORT)}`);
-});
+app.listen(PORT, () => {
+    debug(`listening on port ${chalk.green(PORT)}`);
+  });
--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -1,17 +1,48 @@
 const express = require('express');
-const debug = require('debug')('app:sessionRouter');
-const { MongoClient, ObjectId } = require('mongodb');
+const debug = require('debug')('app:authRouter');
+const { MongoClient, ObjectID } = require('mongodb');
+const passport = require('passport');
 
 const authRouter = express.Router();
 
 authRouter.route('/signUp').post((req, res) => {
-    req.login(req.body, () => {
+  const { username, password } = req.body;
+
+  const url = 'mongodb+srv://dbUser:Hollow_Ichigo8891@cluster0.z6d8o6r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+  const dbName = 'globalterms';
+
+  (async function addUser() {
+    let client;
+    try {
+      client = await MongoClient.connect(url);
+
+      const db = client.db(dbName);
+      const user = { username, password };
+      const results = await db.collection('users').insertOne(user);
+      debug(results);
+      req.login(results.ops[0], () => {
         res.redirect('/auth/profile');
-    });
+      });
+    } catch (error) {
+      debug(error);
+    }
+    client.close();
+  })();
 });
 
+authRouter
+  .route('/signIn')
+  .get((req, res) => {
+    res.render('signin');
+  })
+  .post(
+    passport.authenticate('local', {
+      successRedirect: '/auth/profile',
+      failureRedirect: '/',
+    })
+  );
 authRouter.route('/profile').get((req, res) => {
-    res.json(req.user);
+  res.json(req.user);
 });
 
 module.exports = authRouter;
